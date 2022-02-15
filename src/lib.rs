7ca450d11d61135b6d0753b80c6480b15d552744
Kevin Barclay <barclakj@googlemail.com>
2022-02-15 22:20:01 +0000

    options

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-mod bdes {
+mod baides {
     pub trait Chain {
-        fn head(&self) -> &BdesBlock;
-        fn append(&mut self, block: BdesBlock) -> bool;
+        fn head(&self) -> Option<&BdesBlock>;
+        fn append(&mut self, data: String, metadata: String) -> bool;
         fn length(&self) -> usize;
     }
 
     pub struct BdesBlock {
         pub data: String,
-        pub metadata: String
+        pub metadata: String,
+        pub idx: u64,
+        pub hash: String,
     }
 
     pub struct BdesChain {
@@ -22,11 +24,31 @@ mod bdes {
     }
 
     impl Chain for BdesChain {
-        fn head(&self) -> &BdesBlock {
-            return &self.blocks[self.length()-1];
+        fn head(&self) -> Option<&BdesBlock> {
+            if self.length()>0 {
+                return Some(&self.blocks[self.length()-1]);
+            }
+            None   
         }
 
-        fn append(&mut self, block: BdesBlock) -> bool {
+        fn append(&mut self, data: String, metadata: String) -> bool {
+            let mut idx: u64 = 0;
+            let mut hashable_string: String;
+            let current_head = self.head();
+            match current_head {
+                Some(block) => { 
+                    idx = block.idx + 1; 
+                    hashable_string = block.hash.clone();
+                }, 
+                None => {
+                    idx = 0;
+                    hashable_string = "".to_string();
+                },
+            }
+            // let hash = hmac_sha256::hash("abc")
+            let hash = hashable_string;
+
+            let block = BdesBlock { data: data, metadata: metadata, idx: idx, hash: hash };
             self.blocks.push(block);
             return true;
         }
@@ -40,25 +62,27 @@ mod bdes {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::bdes::Chain;
+    use crate::baides::Chain;
 
     #[test]
     fn create_chain() {
-        let mut chain = bdes::construct_chain("Document");
+        let mut chain = baides::construct_chain("Document");
 
         let data = "hello world".to_string();
         let metadata = "this is some metadata".to_string();
 
-        let block = bdes::BdesBlock { data: data, metadata: metadata };
-
-        chain.append(block);
+        chain.append(data, metadata);
 
         let last_block = chain.head();
 
-        println!("Data: {}", last_block.data);
-
-        assert!(last_block.data.eq("hello world"));
-
+        match last_block {
+            Some(block) => {
+                println!("Data: {}", block.data);
+                assert!(block.data.eq("hello world"));
+            },
+            None => println!("No DATA!")
+        }
+        
         assert!(chain.length()==1);
     }
 }
